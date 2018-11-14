8986584f25f60ad2dec62ba6342d2afc51f517a7
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-14 16:09:48 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -7,6 +7,8 @@ def using_while
 	end
 end
 
+puts using_while
+
 
 # 3. Let's get the first test passing by coding our solution in `while.rb`:
 # Fill out the content of the `using_while` method so that calling it will `puts` the desired phrase while your levitation force is less than `10`. Remember, every time you `puts` the phrase, you should increment your levitation force by `1`.
